170b4a16aafcf3055845c1680152ed5fa60fde9c
marcelinoputra <marcelinoputra0701@gmail.com>
2023-12-01 07:57:51 +0700

    menambahkan javascript server

---
+++ index.js
@@ -0,0 +1,57 @@
+import mysql from 'mysql';
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import crypto from 'crypto';
+import session from 'express-session';
+import multer from 'multer';
+import fs from 'fs';
+
+const PORT = 8050;
+const app = express();
+
+const staticPath = path.resolve('public');
+const assetsPath = path.resolve('assets');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+const upload = multer({ storage: storage });
+
+
+app.use(express.static(staticPath));
+app.use('/assets', express.static(assetsPath));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server is ready, listening in port ${PORT}`);
+});
+
+app.get('/', async (req, res) => {
+    res.render('home');
+});
+
+app.get('/page_dian', async (req, res) => {
+    res.render('page_dian');
+});
+app.get('/page_robertus', async (req, res) => {
+    res.render('page_robertus');
+});
+app.get('/page_yohanes', async (req, res) => {
+    res.render('page_yohanes');
+});
