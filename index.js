759cd1b0daf370a9eaa63a62cab9eb9317dc0ad9
dianestrii <6182101010@student.unpar.ac.id>
2023-12-13 15:16:47 +0700

    Upload CSV js insya allah final

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const pool = mysql.createPool({
 const staticPath = path.resolve('public');
 const assetsPath = path.resolve('assets');
 const uploadPath = path.resolve('uploads');
+const MemoryStore = memorystore(session);
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -43,10 +45,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret: 'secret_key',
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
     resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+    saveUninitialized: false,
+    secret: 'keyboard cat',
 }));
 
 const db = () => {
@@ -141,7 +146,7 @@ app.post('/uploadData', upload.single('file_upload'), (req, res) => {
             Response: row.Response
         };
 
-        const query = 'INSERT INTO `marketing_campaign` SET ?';
+        const query = 'INSERT INTO marketing_campaign SET ?';
         console.log(query);
         conn.query(query, marketing_campaign, (error, results, fields) => {
           if (error) {
@@ -156,99 +161,104 @@ app.post('/uploadData', upload.single('file_upload'), (req, res) => {
   });
 });
 
-app.post('/uploadData', upload.single('file_upload'), async (req, res) => {
-    try {
-        const filePath = path.join(uploadPath, req.file.filename);
+// app.post('/uploadData', upload.single('file_upload'), async (req, res) => {
+//     try {
+//         const filePath = path.join(uploadPath, req.file.filename);
 
-        // Read column headers from CSV
-        const columnHeaders = await readColumnHeaders(filePath);
-        console.log(columnHeaders);
-        // Create table in database
-        await createTableInDatabase(columnHeaders);
+//         // Read column headers from CSV
+//         const columnHeaders = await readColumnHeaders(filePath);
+//         console.log(columnHeaders);
+//         // Create table in database
+//         await createTableInDatabase(columnHeaders);
 
-        // Import data from CSV to the database
-        await importCsvDataToDatabase(filePath, columnHeaders);
+//         // Import data from CSV to the database
+//         await importCsvDataToDatabase(filePath, columnHeaders);
 
-        res.send('File uploaded successfully and table created in the database.');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+//         res.send('File uploaded successfully and table created in the database.');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
 
-const readColumnHeaders = async (filePath) => {
-    return new Promise((resolve, reject) => {
-        const columnHeaders = [];
+// const readColumnHeaders = async (filePath) => {
+//     return new Promise((resolve, reject) => {
+//         const columnHeaders = [];
 
-        fs.createReadStream(filePath)
-            .pipe(csv({ separator: ';' }))
-            .on('data', (row) => {
-                // Assuming the first row of the CSV contains headers
-                if (columnHeaders.length === 0) {
-                    for (const header of Object.keys(row)) {
-                        columnHeaders.push(header.trim());
-                    }
-                    resolve(columnHeaders);
-                }
-            })
-            .on('error', (error) => {
-                reject(error);
-            });
-    });
-};
-
-
-
-
-const createTableInDatabase = async (columnHeaders) => {
-    try {
-        const conn = await db();
-
-        const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS uploaded_data (
-        ${columnHeaders.map(header => `${header} VARCHAR(255)`).join(',\n')}
-    )
-`;
-
-
-        console.log('Create Table Query:', createTableQuery);
-
-        await conn.query(createTableQuery);
-
-        conn.release();
-    } catch (error) {
-        console.error('Error creating table:', error.message);
-        throw error;
-    }
-};
+//         fs.createReadStream(filePath)
+//             .pipe(csv({ separator: ';' }))
+//             .on('data', (row) => {
+//                 // Assuming the first row of the CSV contains headers
+//                 if (columnHeaders.length === 0) {
+//                     for (const header of Object.keys(row)) {
+//                         columnHeaders.push(header.trim());
+//                     }
+//                     resolve(columnHeaders);
+//                 }
+//             })
+//             .on('error', (error) => {
+//                 reject(error);
+//             });
+//     });
+// };
 
 
 
 
-const importCsvDataToDatabase = async (filePath, columnHeaders) => {
-    try {
-        const conn = await db();
+// const createTableInDatabase = async (columnHeaders) => {
+//     try {
+//         const conn = await db();
 
-        const insertQuery = `
-            LOAD DATA LOCAL INFILE ? INTO TABLE uploaded_data
-            FIELDS TERMINATED BY ',' 
-            ENCLOSED BY '"'
-            LINES TERMINATED BY '\\n'
-            IGNORE 1 ROWS
-            (${columnHeaders.join(', ')})
-        `;
+//         const createTableQuery = `
+//     CREATE TABLE IF NOT EXISTS uploaded_data (
+//         ${columnHeaders.map(header => `${header} VARCHAR(255)`).join(',\n')}
+//     )
+// `;
 
-        await conn.query(insertQuery, [filePath]);
 
-        conn.release();
-    } catch (error) {
-        console.error('Error importing data:', error);
-        throw error;
-    }
-};
+//         console.log('Create Table Query:', createTableQuery);
+
+//         await conn.query(createTableQuery);
+
+//         conn.release();
+//     } catch (error) {
+//         console.error('Error creating table:', error.message);
+//         throw error;
+//     }
+// };
+
+
+
+
+// const importCsvDataToDatabase = async (filePath, columnHeaders) => {
+//     try {
+//         const conn = await db();
+
+//         const insertQuery = `
+//             LOAD DATA LOCAL INFILE ? INTO TABLE uploaded_data
+//             FIELDS TERMINATED BY ',' 
+//             ENCLOSED BY '"'
+//             LINES TERMINATED BY '\\n'
+//             IGNORE 1 ROWS
+//             (${columnHeaders.join(', ')})
+//         `;
+
+//         await conn.query(insertQuery, [filePath]);
+
+//         conn.release();
+//     } catch (error) {
+//         console.error('Error importing data:', error);
+//         throw error;
+//     }
+// };
 
 
 //bar chart
 app.get('/barChart', async (req, res) => {
     res.render('barChart');
+});
+
+//scatter plot
+app.get('/scatterPlot', async (req, res) => {
+    res.render('scatterPlot');
 });
